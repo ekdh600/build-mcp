b6225780445b8c87b205e43ce72c4001086794f4
임건 <imgeon@imgeon-ui-MacBookAir.local>
2025-06-30 11:15:24 +0900

    fix: DOCKER_HOST=unix:///var/run/docker.sock 명확히 지정 (DinD+MCP 완전 호환)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+export DOCKER_HOST=unix:///var/run/docker.sock
 dockerd-entrypoint.sh --host=unix:///var/run/docker.sock &
 
 # 도커 데몬이 준비될 때까지 대기 (에러 메시지 출력)
-while ! docker info; do
+while ! DOCKER_HOST=unix:///var/run/docker.sock docker info; do
   echo "Waiting for dockerd..."
   sleep 1
 done
@@ -13,5 +14,4 @@ export HOST=0.0.0.0
 export PORT=3000
 export ENABLE_UNSAFE_SSE_TRANSPORT=1
 export DEBUG='*'
-export DOCKER_HOST=unix:///var/run/docker.sock
 node dist/index.js
